55161d7847e93054283391a0ea73ae95e003a026
Vasu Goel <goelvasu0712@gmail.com>
2019-05-07 00:45:15 +0530

    Add multi_line_strings.py

---
+++ 5. Strings/multi_line_strings.py
@@ -0,0 +1,12 @@
+greeting_message = '''
+
+Hi John,
+
+We have received your purchase request successfully. We'll email you when after the package is dispatched.
+
+Thanks,
+Support Team
+
+'''
+
+print(greeting_message)
