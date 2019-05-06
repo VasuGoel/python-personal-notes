efb5259abf0fc446c625d4d49245910edcc5039c
Vasu Goel <goelvasu0712@gmail.com>
2019-05-07 00:43:54 +0530

    Add age_calculator.py

---
+++ 4. TypeConversions/age_calculator.py
@@ -0,0 +1,9 @@
+birth_year = input('What year were you born in? ')
+print(type(birth_year))
+present_age = 2019 - int(birth_year)
+print(type(present_age))
+
+print('You are', present_age, 'years old!')
+
+
+# int(), float(), bool() are used to convert string to integer, float and booleab respectively
