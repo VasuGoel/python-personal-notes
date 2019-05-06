200a6a708bebc2477e3539fb4461acfde9ec9e48
Vasu Goel <goelvasu0712@gmail.com>
2019-05-07 00:41:15 +0530

    Add variables.py

---
+++ 2. Variables/variables.py
@@ -0,0 +1,21 @@
+# String
+name = 'John Smith'
+
+# Integer
+age = 20
+
+# Float
+rating = 4.9
+
+# Boolean
+is_Patient = True
+
+print('Patient info :\n')
+print('Name -', name)
+print('Age -', age)
+print('Rating -', rating)
+
+if is_Patient:
+    print('New patient')
+else:
+    print('Old patient')
