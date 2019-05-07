b26321916336b045556fe617c1c174c06f837820
Vasu Goel <goelvasu0712@gmail.com>
2019-05-07 17:59:59 +0530

    Add operator_precedence.py

---
+++ 6. ArithmeticOperations/operator_precedence.py
@@ -0,0 +1,6 @@
+value = (2 + 3) ** 5 - 4 + 7 * 10 / 5 ** 2
+
+print(value)        # returns 3123.8
+
+# Order of precedence (left to right)
+# (Paranthesis) > Exponential > Multiply or Divide > Add or Subtract
