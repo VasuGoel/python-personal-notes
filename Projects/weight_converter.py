3754cc9e1501c23a47052641d7d318016ad64485
Vasu Goel <goelvasu0712@gmail.com>
2019-05-07 23:51:24 +0530

    Change conditional to unit.upper()

--- Projects/weight_converter.py
+++ Projects/weight_converter.py
@@ -1,8 +1,8 @@
 weight = float(input('Weight: '))
 unit = input('(L)bs or (K)g: ')
-if unit == 'l' or unit == 'L':
+if unit.upper() == 'L':
     print(f'You are {weight * 0.45} kilos')
-elif unit == 'k' or unit =='K':
+elif unit.upper() == 'K':
     # ":.2f".format(var) is used to format floating point output upto 2 decimal places. While ":10.2f".format(var) adds 10 empty space and then display floating point number upto 2 decimal places
     print(f'You are {"{:.2f}".format((weight / 0.45))} pounds')
 else:
