f152594ea470c2af7101c123319994df50238268
Vasu Goel <goelvasu0712@gmail.com>
2019-05-07 18:00:14 +0530

    Add math_functions.py

---
+++ 6. ArithmeticOperations/math_functions.py
@@ -0,0 +1,17 @@
+# Python has inbuilt math functions for fairly difficult problems but for complex calculations we need to import a separate 'math module'.
+# Module in python is a separate file with some reusable code. We use modules to organize our code into different files
+
+import math
+
+x = 6.9
+y = -6.9
+
+# Inbuilt math functions
+print('After rounding up -', round(x))
+print('Absolute value -', abs(y))
+
+# Math module functions
+print('Floor of x -', math.floor(x))
+print('Ceil of x -', math.ceil(x))
+print('Floor of y -', math.floor(y))
+print('Ceil of y -', math.ceil(y))
