7b1e8e20c2ebed1fde88ef5d508ec19f7b4616d7
Vasu Goel <goelvasu0712@gmail.com>
2019-05-07 18:01:05 +0530

    Add conditionals.py

---
+++ 7. Conditionals/conditionals.py
@@ -0,0 +1,13 @@
+is_Hot = False
+is_Cold = True
+
+if is_Hot:
+     print('It\'s a hot day')
+     print('Drink plenty of water')
+elif is_Cold:
+    print('It\'s a cold day')
+    print('Wear warm clothes')
+else:
+    print('It\'s a lovely day')
+
+print('Enjoy you day!')
