06d154a5f72f2b834aa58fd13cae6292a4f11a63
Vasu Goel <goelvasu0712@gmail.com>
2019-05-07 23:48:12 +0530

    Add weight_converter.py to Projects/

---
+++ Projects/weight_converter.py
@@ -0,0 +1,9 @@
+weight = float(input('Weight: '))
+unit = input('(L)bs or (K)g: ')
+if unit == 'l' or unit == 'L':
+    print(f'You are {weight * 0.45} kilos')
+elif unit == 'k' or unit =='K':
+    # ":.2f".format(var) is used to format floating point output upto 2 decimal places. While ":10.2f".format(var) adds 10 empty space and then display floating point number upto 2 decimal places
+    print(f'You are {"{:.2f}".format((weight / 0.45))} pounds')
+else:
+    print('Invalid choice. Choose either (L)bs or (K)g.')
