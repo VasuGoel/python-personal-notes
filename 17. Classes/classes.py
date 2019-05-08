bea83923b26ac2453f088a6e6daf96114caef9da
Vasu Goel <goelvasu0712@gmail.com>
2019-05-08 20:28:02 +0530

    Add classes.py

---
+++ 17. Classes/classes.py
@@ -0,0 +1,23 @@
+class Point:
+    def move(self):
+        print('Moving point...')
+
+    def draw(self):
+        print('Drawing point...')
+
+
+p1 = Point()    # Creating an object of class Point
+p1.move()
+p1.draw()
+
+# Defining atributes that belong to particular object
+p1.x = 10
+p1.y = 20
+print(f'p1(x, y): ({p1.x}, {p1.y})\n')
+
+
+# Since, each object is a different instance of Point class, atributes of point 1 won't be accessible in point 2
+p2 = Point()
+p2.move()
+p2.draw()
+# print(f'p2(x, y): ({p2.x}, {p2.y})')        AttributeError: 'Point' object has no attribute 'x'
