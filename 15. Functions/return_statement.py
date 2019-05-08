9f9c862375dcdbdf6a3a0074969f83bf566d5c28
Vasu Goel <goelvasu0712@gmail.com>
2019-05-08 20:10:19 +0530

    Add return_statement.py

---
+++ 15. Functions/return_statement.py
@@ -0,0 +1,7 @@
+def square(n):
+    return n*n
+
+print(square(4))
+
+# By default, Python returns None for functions that don't have a return statement
+# None object signifies the absense of value or 'null' in java or c++ or javascript
