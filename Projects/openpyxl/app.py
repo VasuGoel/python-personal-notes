6f21b8d7bde4bbda3a09f0183181967844a06f03
Vasu Goel <goelvasu0712@gmail.com>
2019-05-23 01:36:12 +0530

    Add barchart and reference

--- Projects/openpyxl/app.py
+++ Projects/openpyxl/app.py
@@ -11,11 +11,17 @@ print(f'\nProcessing {sheet} from \'{wb}\'... \n')
 for row in range(2, sheet.max_row + 1):
     cell = sheet.cell(row, 3)       # sheet['a1'] is another way to access a cell
     print(cell.value)
-    discounted_price = cell.value * 0.9
+    discounted_price = cell.value * 0.5
     discounted_price_cell = sheet.cell(row, 4)
     discounted_price_cell.value = discounted_price
 
+# Returns values of column 4 and rows 2 to max_row as an object
+values = Reference(sheet, min_row=2, max_row=sheet.max_row, min_col=4, max_col=4)
 
+# Create instance of BarChart and pass 'values' to chart and add chart to sheet at column 'E2'
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, 'E2')
 
 wb.save('transactions2.xlsx')
-print('\nDone.\n')
+print('\nFile Saved.\n')
